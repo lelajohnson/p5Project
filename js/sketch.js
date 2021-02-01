0e4cc39bf013bbb92858b1196fca2982cb19183b
lelajohnson <lsj396@gmail.com>
2021-02-01 17:51:00 -0600

    More edits

--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,38 @@
+let ellipseX = 0;
+let ellipseY = 0;
+let claire;
+
 function setup() {
   createCanvas(windowWidth, windowWidth);
 
   background(180, 60, 220);
 
-  print(windowWidth);
   //tells us the window width
+
+  function preload(){
+      claire = loadImage("images/claire.jpg");
+
+  }
+
+  imageMode(CENTER);
 
   rectMode(CENTER);
   //change rect x, y coordinates to center
+
+  ellipseX = random(ellipseX, windowWidth);
+  ellipseY = random(ellipseY, windowHeight);
+
+  print(ellipseX);
 }
 
 function draw() {
   // put drawing code here
   fill(120);
   //affects everything after it
-  stroke(40, 200, 100);
+  stroke(random(40), random(200), random(100));
   strokeWeight(10);
   //affects stroke weight
-  ellipse(300, 500, 50, 50);
+  ellipse(ellipseX, ellipseY, 50, 50);
   point(mouseX, mouseY);
   rect(800, 300, 200, 300);
   //x, y, width, height
@@ -25,4 +40,8 @@ function draw() {
   triangle(1200, 50, 1300, 600, 950, 400);
 
   print(mouseX);
+  strokeWeight(10);
+  line(30, 200, 800, 600);
+
+  image(claire, windowWidth/2, windowHeight/2);
 }
