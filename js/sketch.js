3d74466ce3fb43bf1091f3581371867a756ce562
lelajohnson <lsj396@gmail.com>
2021-02-01 11:35:58 -0600

    Ellipse added

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowWidth);
+
+  background(180, 60, 220);
+
+  print(windowWidth);
 }
 
 function draw() {
   // put drawing code here
+  ellipse(300, 500, 50, 50);
 }
